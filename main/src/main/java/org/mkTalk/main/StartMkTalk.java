5a77b4d93bed18bfaf747b78ec679f965deceb43
houk1987 <houk1987@gmail.com>
2016-07-24 18:13:45 +0800

    mkTalk

---
+++ main/src/main/java/org/mkTalk/main/StartMkTalk.java
@@ -0,0 +1,20 @@
+package org.mkTalk.main;
+
+/**
+ * 作者：hk on 16/7/24 17:19
+ * <p>
+ * 邮箱：houk1987@gmail.com
+ */
+public class StartMkTalk {
+
+	private String name;
+	private String pwd;
+
+
+
+	public static void main(String[] args) {
+
+	}
+
+
+}
